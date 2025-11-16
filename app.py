8f9b39eb37a9732129dd2be95887d601a928da63
rajkumar <rkth123456@gmail.com>
2025-11-16 01:28:49 +0000

    i have updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"message":"hello from rajkumar-devosp","status":"ok"})
+    return jsonify({"message":"hello from rajkumar-devops","status":"ok"})
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", port=5000)
